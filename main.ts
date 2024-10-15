95416567157f796db4f785f07869ac9879554da9
sehdevkumar <kumarsehdev600@gmail.com>
2024-10-15 07:18:47 +0530

    fun with deno

---
+++ main.ts
@@ -0,0 +1,15 @@
+export const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+console.log(add(2, 3));
+
+try {
+  // Ensure you're reading the correct directory and file
+  const entries = Deno.readDirSync(".");
+  for (const entry of entries) {
+    console.log(entry.name);
+  }
+} catch (error) {
+  console.error("Error reading directory:", error);
+}
